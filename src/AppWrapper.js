e4cb07e0baecb6f8a2aa42beb4e9e3c3dfc3cd60
JuanYanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-08-01 00:30:47 -0500

    cambios
    cambios

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
+import keycloak from './Keycloak';
 import App from './App';
 import Login from './pages/Login';
 import Error from './pages/Error';
@@ -8,22 +9,39 @@ import Access from './pages/Access';
 import Landing from './pages/Landing';
 
 const AppWrapper = (props) => {
-    let location = useLocation();
+  const [authenticated, setAuthenticated] = useState(false);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [location]);
+  useEffect(() => {
+    keycloak.init({ onLoad: 'check-sso', promiseType: 'native' })
+      .then((authenticated) => {
+        setAuthenticated(authenticated);
+      })
+      .catch((error) => {
+        console.error('Error initializing Keycloak: ', error);
+      });
+  }, []);
 
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/error" element={<Error />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/access" element={<Access />} />
-            <Route path="/landing" element={<Landing />} />
-            <Route path="*" element={<App />} />
-        </Routes>
-    );
+  let location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  if (!authenticated) {
+    // Redirect to login page if not authenticated
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="/notfound" element={<NotFound />} />
+      <Route path="/access" element={<Access />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="*" element={<App />} />
+    </Routes>
+  );
 };
 
 export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import AppWrapper from './AppWrapper';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <HashRouter>
-            <AppWrapper></AppWrapper>
-        </HashRouter>
-    </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <AppWrapper />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
