71d48265fc14ab96b368f4b7510863a992408abd
JuanYanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-08-02 03:47:54 -0500

    subir
    subir

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
-import keycloak from './Keycloak';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import App from './App';
 import Login from './pages/Login';
 import Error from './pages/Error';
@@ -9,39 +8,22 @@ import Access from './pages/Access';
 import Landing from './pages/Landing';
 
 const AppWrapper = (props) => {
-  const [authenticated, setAuthenticated] = useState(false);
+    let location = useLocation();
 
-  useEffect(() => {
-    keycloak.init({ onLoad: 'check-sso', promiseType: 'native' })
-      .then((authenticated) => {
-        setAuthenticated(authenticated);
-      })
-      .catch((error) => {
-        console.error('Error initializing Keycloak: ', error);
-      });
-  }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
 
-  let location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
-
-  if (!authenticated) {
-    // Redirect to login page if not authenticated
-    return <Navigate to="/login" />;
-  }
-
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/error" element={<Error />} />
-      <Route path="/notfound" element={<NotFound />} />
-      <Route path="/access" element={<Access />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="*" element={<App />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/error" element={<Error />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/access" element={<Access />} />
+            <Route path="/landing" element={<Landing />} />
+            <Route path="*" element={<App />} />
+        </Routes>
+    );
 };
 
 export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,51 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import AppWrapper from './AppWrapper';
 import reportWebVitals from './reportWebVitals';
+import Keycloak from 'keycloak-js';
+const _kc = new Keycloak('/keycloak.json');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <AppWrapper />
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const keycloakConfig = {
+    realm: "react-keyclock",
+    url: "http://127.0.0.1:8080/auth/",
+    clientId: "restcli",
+    clientSecret: "7da19512-f77d-45de-898f-de276c643875",
+    beareronly: true,
+    onLoad: 'login-required',
+  };
+  
 
-reportWebVitals();
+
+const initKeycloak = () => {
+    const keycloak = new Keycloak(keycloakConfig);
+    return new Promise((resolve, reject) => {
+      keycloak.init({ onLoad: 'login-required' })
+        .then((authenticated) => {
+          if (authenticated) {
+            resolve(keycloak);
+          } else {
+            reject(new Error('User not authenticated'));
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+  
+  initKeycloak()
+    .then((keycloak) => {
+      const root = ReactDOM.createRoot(document.getElementById('root'));
+      root.render(
+        <React.StrictMode>
+          <HashRouter>
+            <AppWrapper />
+          </HashRouter>
+        </React.StrictMode>
+      );
+      
+      reportWebVitals();
+    })
+    .catch((error) => {
+      console.error('Error initializing Keycloak:', error);
+      
+    });
